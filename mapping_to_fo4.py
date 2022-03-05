c3e5fc1735e5cdbcfc48782acd2ed0cf2d957554
Phileas Fogg <phileas.fogg@mail.com>
2022-03-05 13:49:11 +0100

    Improve prints and bug found (not fixed)

--- mapping_to_fo4.py
+++ mapping_to_fo4.py
@@ -148,8 +148,18 @@ with open(sys.argv[1]) as json_file:
     print(module_name)
     module = data["modules"][module_name]
 
-    # Build a list which maps connections to cells with inputs connected to this connection
-    # order all connections with they number (easier to navigate)
+    def pin_pretty_name(pin_number):
+        for netname in module["netnames"]:
+            net = module["netnames"][netname]
+            counter = 0
+            if pin_number in net["bits"]:
+                return "{}[{}] (net {})".format(netname, counter, pin_number)
+                counter += 1
+        print("Error: Cannot find net (aka connection) %d in the netnames".format(
+            pin_number))
+
+        # Build a list which maps connections to cells with inputs connected to this connection
+        # order all connections with they number (easier to navigate)
     connections = {}
     # {42: [Cell_0, Cell_1, Cell_3], 23: ...}
     for cellname in get_cells():
@@ -184,7 +194,7 @@ with open(sys.argv[1]) as json_file:
             for pin in pins:
                 if not pin in connections:
                     print(
-                        "Warning: Module's input pin %d is not connected to any cell's input" % pin)
+                        "Warning: Module's input pin %s is not connected to any cell's input" % pin_pretty_name(pin))
                     continue
                 for cell in connections[pin]:
                     if not cell in list_of_cells_to_propagate:
@@ -216,14 +226,19 @@ with open(sys.argv[1]) as json_file:
             for cell_output_pin in list_cell_outputs(cell):
                 # Open output, cannot propagate further
                 cell_output_pin_open = not cell_output_pin in connections
-                if cell_output_pin_open or is_pin_output(cell_output_pin):
-                    print("Info: %s output pin %d is not connected to any cell's input" % (
-                        cell["name"], cell_output_pin))
+                cell_output_pin_is_output = is_pin_output(cell_output_pin)
+                if cell_output_pin_open or cell_output_pin_is_output:
+                    if cell_output_pin_is_output:
+                        print("Done propagating to output pin {}, last cell was {}".format(
+                            pin_pretty_name(cell_output_pin), cell['name']))
+                    else:
+                        print("Warning: %s is not connected to any other cell, last cell was %s" % (
+                            pin_pretty_name(cell_output_pin), cell["name"]))
 
                     # error detection
                     if cell_output_pin in output_pin_max_fo4:
                         print(
-                            "Error: We are already done processing output pin %d but you try to duplicate it's entry" % cell_output_pin)
+                            "Error: We are already done processing output pin %s but you try to duplicate it's entry" % pin_pretty_name(cell_output_pin))
                     assert(not cell_output_pin in output_pin_max_fo4)
 
                     output_pin_max_fo4[cell_output_pin] = cell["max_prop_fo4"]
@@ -256,11 +271,12 @@ with open(sys.argv[1]) as json_file:
         #    len(new_list_of_cells_to_propagate)))
 
     print("List of output signals and Min/Max FO4")
-    print("Pin | max FO4 | max chain | min F04 | min chain | name")
+    print("max FO4 | max chain | min F04 | min chain | name")
     for i in output_pin_last_cell:
-        print("{: >3} | {:7.2f} | {: >9} | {:7.2f} | {: >9} | {}".format(
-            i, output_pin_max_fo4[i], output_pin_max_cell[i], output_pin_min_fo4[i],
-            output_pin_min_cell[i], output_pin_last_cell[i]["name"]
+        print("{: >3} | {:7.2f} | {: >9} | {:7.2f} | {: >9} | {} from cell {}".format(
+            output_pin_max_fo4[i], output_pin_max_cell[i], output_pin_min_fo4[i],
+            output_pin_min_cell[i], pin_pretty_name(i),
+            output_pin_last_cell[i]["name"]
         ))
 
     print("\nFO4 Summary:")
@@ -270,8 +286,8 @@ with open(sys.argv[1]) as json_file:
         if output_pin_max_fo4[i] > max_fo4:
             max_fo4 = output_pin_max_fo4[i]
             max_fo4_index = i
-    print("Max FO4: {} from cell {}".format(
-        max_fo4, output_pin_last_cell[max_fo4_index]["name"]))
+    print("Max FO4: {} on signal {} from cell {}".format(
+        max_fo4, pin_pretty_name(max_fo4_index), output_pin_last_cell[max_fo4_index]["name"]))
     print(
         "\t- max F04 on the same signal: {}".format(output_pin_max_fo4[max_fo4_index]))
     print(
@@ -287,8 +303,8 @@ with open(sys.argv[1]) as json_file:
         if output_pin_min_fo4[i] < min_fo4:
             min_fo4 = output_pin_min_fo4[i]
             min_fo4_index = i
-    print("Min FO4: {} from cell {}".format(
-        min_fo4, output_pin_last_cell[min_fo4_index]["name"]))
+    print("Min FO4: {} on signal {} from cell {}".format(
+        min_fo4, pin_pretty_name(min_fo4_index), output_pin_last_cell[min_fo4_index]["name"]))
     print(
         "\t- max F04 on the same signal: {}".format(output_pin_max_fo4[min_fo4_index]))
     print(
